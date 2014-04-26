aeb41e3369970c8c02d45e8c1efdc6b231543a5e
KatieK <KatieK2@users.noreply.github.com>
2014-04-25 19:19:25 -0700

    Update autorun.bat

--- autorun.bat
+++ autorun.bat
@@ -1,6 +1,6 @@
 REM   Open the registry editor by searching for regedit.
 REM   Find this key: HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Command Processor
-REM   Create a new name called AutoRun, and set its value to the location of this file, ex "C:\Users\[username]\autorun.bat"
+REM   Create a new key called AutoRun, and set its value to the location of this file, ex "C:\Users\[username]\autorun.bat"
 
 @ECHO OFF
 doskey ..=cd..
